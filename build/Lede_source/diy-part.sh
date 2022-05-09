c8b4d71b6f940e802074f5a33eef42904ba3181a
byff <535937737@qq.com>
2022-05-09 22:04:55 +0800

    增加插件

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -2,6 +2,7 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展 在此脚本 增加插件
 
+git clone https://github.com/kenzok8/openwrt-packages package/gd772
 wget https://raw.githubusercontent.com/gd0772/patch/main/gd772.sh
 bash gd772.sh
 sed -i 's/02b79d5e2b07b5e64cd28f1fe84395ee11eef95fc49fd923a9ab93022b148be6/skip/g' feeds/packages/utils/containerd/Makefile
